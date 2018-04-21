0c95cb2f41f4dfac95f12ba1c6315b045845ba8b
Soojin <soo.kim@ucdenver.edu>
2018-04-20 20:56:22 -0600

    changes for hw5

--- server.js
+++ server.js
@@ -169,12 +169,11 @@ router.route('/reviews/insert/:title')
     .post(authJwtController.isAuthenticated, function (req, res) {
         Movie.findOne({title: req.params.title}).exec(function(err, movie){
             if (movie !== null){
-                Review.count({movie: req.params.title}).exec(function(err, numReview){
-                    if (err) {
-                        return res.send(err);
-                    }
+                var numReview = 0;
+                Review.count({ movie: req.params.title}, function(err, review){
+                    if (err) return (res.send(err));
+                    numReview = review;
                 });
-                console.log(numReview);
                 var newReview = new Review(req, res);
                 newReview.username = signinUser;
                 newReview.review = req.body.review;
@@ -188,15 +187,6 @@ router.route('/reviews/insert/:title')
                         else
                             return res.send(err);
                     }
-
-                    // var newRating = (movie.avgRating + newReview.rating) / (numReview + 1);
-                    //
-                    // movie.avgRating = newRating;
-                    // movie.update(function(err){
-                    //     if (err){
-                    //         return res.send(err);
-                    //     }
-                    // });
 
                     res.json({ msg: 'Review insert success.' });
                 });
